c34592d6ca04a056fa3d8c38e99d478fbb62b35e
João Paulo <jpsouza.rod@gmail.com>
2021-06-14 23:25:46 -0300

    new update

--- functions.rb
+++ functions.rb
@@ -1,82 +1,44 @@
-def add_contact(name)
-  File.open('catalogo.txt', 'r') do |file|
-    $name_exist = file.read.match?(/#{name}\b/i)
-  end
+require_relative 'functions'
+require 'colorize'
+
+loop do
+  puts 'Bem vindo ao catalogo Telefonico'
+  puts '[1] Adicionar contato'.blue
+  puts '[2] Pesquisar contato'.blue
+  puts '[3] listar contatos'.blue
+  puts '[4] Apagar um contato'.blue
+  puts '[5] Sair'.blue
+  print '> '
+  func = gets.chomp.to_i
+
+  case func
+  when 1
+    puts 'Adicionar contato'
+    puts 'Digite o nome do contato'.blue
+    name = gets.chomp.to_s.capitalize()
+    add_contact(name)
+
+  when 2
+    puts 'Digite o contato a ser pesquisado'.blue
+    name = gets.chomp.to_s.capitalize()
+    search_by_name(name)
+
+  when 3
+    puts 'lista de contatos'
     File.open('catalogo.txt', 'r') do |file|
-      if $name_exist
-        puts 'Contato existente'.red
-        puts 'Digite um novo contato'.blue
-        name = gets.chomp.to_s
-        add_contact(name)
-      else
-        puts 'Digite o email do contato'.blue 
-        email = gets.chomp.to_s
-        puts 'Digite uma etiqueta para o contato'.blue
-        etiqueta = gets.chomp.to_s
-        puts 'Digite o telefone do contato'.blue
-        tel = gets.chomp.to_i
-
-        if tel.to_s.length == 11 
-          File.open('catalogo.txt', 'a') do |file|
-          file.write("\n#{name}, #{email}, #{etiqueta}, #{tel}")
-          puts 'Contato adicionado com sucesso'.green
-          end
-        else
-          puts 'Entrada de número inválida'.red
-        end   
-      end
+      puts file.readline('catalogo.txt').yellow
     end
-end
 
-def search_by_name(name)
-    File.open('catalogo.txt', 'r') do |file|
-        if file.read.match?(/#{name}\b/i)
-          lines = IO.readlines('catalogo.txt')
-          for i in lines
-            if i.include?(name)
-              puts i.yellow
-            end
-          end   
-        else
-          puts 'Contato não encontrado'.red
-        end
-    end
-end
-
-def contact_delete(name)
-  empty_text = ""
-  suggest_contacts = []
-
-  File.open('catalogo.txt').each do |line|
-    line_array = line.split(',')
-    full_contact_name = line_array[0]
-    
-    if name.downcase == full_contact_name.downcase or full_contact_name.downcase.include?(name.downcase)
-      suggest_contacts.push(full_contact_name)
-    end
+  when 4
+    puts 'Apagar contatos'
+    puts 'Digite o nome do contato a ser excluido'.blue
+    name = gets.chomp.to_s.capitalize()
+    contact_delete(name)
+  when 5
+    break
+  else
+    puts 'Entrada Inválida !!!'.red
   end
-
-  if suggest_contacts.length > 1 
-    puts "Você quis dizer algum desses nomes?"
-    for i in suggest_contacts
-      puts i.yellow
-    end
-
-    reset_name = gets.chomp.to_s
-
-    contact_delete(reset_name.downcase)
-  elsif suggest_contacts.length == 1
-    File.open('catalogo.txt').each do |line|
-      if line.split(',')[0].downcase != suggest_contacts[0].downcase
-        empty_text += line
-      end
-    end
-
-    write_file = File.new('catalogo.txt', 'w')
-    write_file.write(empty_text)
-    write_file.close
-    puts "Contato excluido com sucesso".green
-  else 
-    puts "Nenhum nome corresponde ao informado".red
-  end
+  system('pause')
+  system('cls')
 end
